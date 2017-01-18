b3a1764ca7a1b27efd4137d1fe51dc398dd33037
Max Mitchell <max@maxis.me>
2017-01-18 13:21:20 +0000

    Better comments

--- amazon-bash.sh
+++ amazon-bash.sh
@@ -5,7 +5,7 @@ START=$(date +%s);
 tcpdump -l -i eth0 ether host "$MAC" | while read b; do
         END=$(date +%s);
         diff=`echo $((END-START)) | awk 'int($1%60)'` # Get time since last click.
-        if [ $(($diff)) -gt 3 ] # as tcdump sends multiple outputs.
+        if [ $(($diff)) -gt 2 ] # 2 seconds since last click. As tcdump sends multiple outputs.
         then
                 START=$(date +%s)
                 
