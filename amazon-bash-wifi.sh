581784a7eaf6155d7bbe8df9ca325eab930d5a45
Max Mitchell <max@maxis.me>
2017-01-26 18:45:03 +0000

    Add onClick function

--- amazon-bash-wifi.sh
+++ amazon-bash-wifi.sh
@@ -1,20 +1,23 @@
 #!/bin/bash
 MAC="<AMAZON DASH MAC ADDRESS>"
 
-if [ -z "$(ifconfig | grep 'mon0')" ]
+function onClick {
+    #code to be ran on click
+    echo "Ding Dong"
+}
+
+if [ -z "$(ifconfig | grep 'mon0')" ] # if not already up
 then
-        airmon-ng start wlan0
+    airmon-ng start wlan0
 fi
 
 START=$(date +%s);
 tcpdump -l -i mon0 ether host "$MAC" | while read b; do
-        END=$(date +%s);
-        diff=`echo $((END-START)) | awk 'int($1%60)'` # Get time since last click.
-        if [ $(($diff)) -gt 3 ] # wait 3 seconds after last click - tcdump has multiple outputs.
-        then
-                START=$(date +%s)
-
-                # Performed on click
-                echo "Ding Dong"
-        fi
+    END=$(date +%s);
+    diff=`echo $((END-START)) | awk 'int($1%60)'` # Get time since last click.
+    if [ $(($diff)) -gt 3 ] # wait more than 3 seconds after last click - tcdump has multiple outputs.
+    then
+        START=$(date +%s)
+        onClick & # run in background
+    fi
 done
--- amazon-bash.sh
+++ amazon-bash.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 MAC="<AMAZON DASH MAC ADDRESS>"
 
+function onClick {
+    #code to be ran on click
+    echo "Ding Dong"
+}
+
 START=$(date +%s);
 tcpdump -l -i eth0 ether host "$MAC" | while read b; do
-        END=$(date +%s);
-        diff=`echo $((END-START)) | awk 'int($1%60)'` # Get time since last click.
-        if [ $(($diff)) -gt 3 ] # wait 3 seconds after last click - tcdump has multiple outputs.
-        then
-                START=$(date +%s)
-
-                # Performed on click
-                echo "Ding Dong"
-        fi
+    END=$(date +%s);
+    diff=`echo $((END-START)) | awk 'int($1%60)'` # Get time since last click.
+    if [ $(($diff)) -gt 3 ] # wait more than 3 seconds after last click - tcdump has multiple outputs.
+    then
+        START=$(date +%s)
+        onClick & # run in background
+    fi
 done
